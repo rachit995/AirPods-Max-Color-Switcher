11bbbbcaf1f43e441e97a946d2a49d66f92f2b39
Rachit Sharma <rachit995@rocketmail.com>
2020-12-16 02:06:50 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
+import images from './assets/images'
+import { FaTwitter } from 'react-icons/fa'
 
 function App() {
+  const [headPhoneColor, setHeadPhoneColor] = useState(0)
+  const [rightColor, setRightColor] = useState(0)
+  const [leftColor, setLeftColor] = useState(0)
+  const imageUrl = images[`s${headPhoneColor}${leftColor}${rightColor}`]
+  function generateOptions() {
+    return (
+      <>
+        <option value='0'>Space Grey</option>
+        <option value='1'>Silver</option>
+        <option value='2'>Green</option>
+        <option value='3'>Sky Blue</option>
+        <option value='4'>Pink</option>
+      </>
+    )
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className='app__container'>
+      <div className="app__leftColumn">
+        <img src={imageUrl} alt='' className='app__AirPodsMaxImage' />
+      </div>
+      <div className="app__rightColumn">
+        <div>
+          <div class="app__lc_new">
+            New
+            </div>
+          <h1 class="app__lc_h1">
+            AirPods Max
+          </h1>
+        </div>
+        <h1 class="app__lc_h2">
+          Body Color
+        </h1>
+        <select onChange={(e) => { setHeadPhoneColor(e.target.value) }}>
+          {generateOptions()}
+        </select>
+        <h1 class="app__lc_h2">
+          Left Ear Cushion
+        </h1>
+        <select onChange={(e) => { setLeftColor(e.target.value) }}>
+          {generateOptions()}
+        </select>
+        <h1 class="app__lc_h2">
+          Right Ear Cushion
+        </h1>
+        <select onChange={(e) => { setRightColor(e.target.value) }}>
+          {generateOptions()}
+        </select>
+      </div>
+      <div className="app__footer">
+        <a href="https://bit.ly/3h2bqRb" target="_blank" rel="noreferrer">
+          <FaTwitter />
+          <h4>
+            @rachit995
+        </h4>
         </a>
-      </header>
+      </div>
     </div>
   );
 }
---
+++ src/assets/images/index.js
@@ -0,0 +1,251 @@
+import s111 from './1-1-1.jpg'
+import s213 from './2-1-3.jpg'
+import s211 from './2-1-1.jpg'
+import s210 from './2-1-0.jpg'
+import s212 from './2-1-2.jpg'
+import s214 from './2-1-4.jpg'
+import s201 from './2-0-1.jpg'
+import s203 from './2-0-3.jpg'
+import s200 from './2-0-0.jpg'
+import s202 from './2-0-2.jpg'
+import s204 from './2-0-4.jpg'
+import s221 from './2-2-1.jpg'
+import s223 from './2-2-3.jpg'
+import s220 from './2-2-0.jpg'
+import s222 from './2-2-2.jpg'
+import s224 from './2-2-4.jpg'
+import s243 from './2-4-3.jpg'
+import s241 from './2-4-1.jpg'
+import s404 from './4-0-4.jpg'
+import s400 from './4-0-0.jpg'
+import s402 from './4-0-2.jpg'
+import s401 from './4-0-1.jpg'
+import s403 from './4-0-3.jpg'
+import s240 from './2-4-0.jpg'
+import s242 from './2-4-2.jpg'
+import s230 from './2-3-0.jpg'
+import s411 from './4-1-1.jpg'
+import s232 from './2-3-2.jpg'
+import s234 from './2-3-4.jpg'
+import s413 from './4-1-3.jpg'
+import s410 from './4-1-0.jpg'
+import s412 from './4-1-2.jpg'
+import s414 from './4-1-4.jpg'
+import s233 from './2-3-3.jpg'
+import s244 from './2-4-4.jpg'
+import s314 from './3-1-4.jpg'
+import s231 from './2-3-1.jpg'
+import s312 from './3-1-2.jpg'
+import s310 from './3-1-0.jpg'
+import s313 from './3-1-3.jpg'
+import s434 from './4-3-4.jpg'
+import s424 from './4-2-4.jpg'
+import s420 from './4-2-0.jpg'
+import s440 from './4-4-0.jpg'
+import s421 from './4-2-1.jpg'
+import s441 from './4-4-1.jpg'
+import s443 from './4-4-3.jpg'
+import s442 from './4-4-2.jpg'
+import s444 from './4-4-4.jpg'
+import s430 from './4-3-0.jpg'
+import s433 from './4-3-3.jpg'
+import s431 from './4-3-1.jpg'
+import s432 from './4-3-2.jpg'
+import s423 from './4-2-3.jpg'
+import s422 from './4-2-2.jpg'
+import s110 from './1-1-0.jpg'
+import s112 from './1-1-2.jpg'
+import s114 from './1-1-4.jpg'
+import s113 from './1-1-3.jpg'
+import s101 from './1-0-1.jpg'
+import s100 from './1-0-0.jpg'
+import s102 from './1-0-2.jpg'
+import s104 from './1-0-4.jpg'
+import s103 from './1-0-3.jpg'
+import s121 from './1-2-1.jpg'
+import s120 from './1-2-0.jpg'
+import s122 from './1-2-2.jpg'
+import s124 from './1-2-4.jpg'
+import s123 from './1-2-3.jpg'
+import s141 from './1-4-1.jpg'
+import s140 from './1-4-0.jpg'
+import s142 from './1-4-2.jpg'
+import s144 from './1-4-4.jpg'
+import s143 from './1-4-3.jpg'
+import s131 from './1-3-1.jpg'
+import s130 from './1-3-0.jpg'
+import s132 from './1-3-2.jpg'
+import s134 from './1-3-4.jpg'
+import s133 from './1-3-3.jpg'
+import s011 from './0-1-1.jpg'
+import s010 from './0-1-0.jpg'
+import s012 from './0-1-2.jpg'
+import s014 from './0-1-4.jpg'
+import s013 from './0-1-3.jpg'
+import s001 from './0-0-1.jpg'
+import s000 from './0-0-0.jpg'
+import s002 from './0-0-2.jpg'
+import s004 from './0-0-4.jpg'
+import s003 from './0-0-3.jpg'
+import s021 from './0-2-1.jpg'
+import s020 from './0-2-0.jpg'
+import s022 from './0-2-2.jpg'
+import s024 from './0-2-4.jpg'
+import s023 from './0-2-3.jpg'
+import s041 from './0-4-1.jpg'
+import s040 from './0-4-0.jpg'
+import s042 from './0-4-2.jpg'
+import s044 from './0-4-4.jpg'
+import s033 from './0-3-3.jpg'
+import s031 from './0-3-1.jpg'
+import s030 from './0-3-0.jpg'
+import s032 from './0-3-2.jpg'
+import s034 from './0-3-4.jpg'
+import s301 from './3-0-1.jpg'
+import s300 from './3-0-0.jpg'
+import s302 from './3-0-2.jpg'
+import s304 from './3-0-4.jpg'
+import s303 from './3-0-3.jpg'
+import s321 from './3-2-1.jpg'
+import s320 from './3-2-0.jpg'
+import s322 from './3-2-2.jpg'
+import s324 from './3-2-4.jpg'
+import s323 from './3-2-3.jpg'
+import s341 from './3-4-1.jpg'
+import s340 from './3-4-0.jpg'
+import s342 from './3-4-2.jpg'
+import s344 from './3-4-4.jpg'
+import s343 from './3-4-3.jpg'
+import s331 from './3-3-1.jpg'
+import s330 from './3-3-0.jpg'
+import s332 from './3-3-2.jpg'
+import s334 from './3-3-4.jpg'
+import s333 from './3-3-3.jpg'
+import s311 from './3-1-1.jpg'
+
+export default {
+  s111,
+  s213,
+  s211,
+  s210,
+  s212,
+  s214,
+  s201,
+  s203,
+  s200,
+  s202,
+  s204,
+  s221,
+  s223,
+  s220,
+  s222,
+  s224,
+  s243,
+  s241,
+  s404,
+  s400,
+  s402,
+  s401,
+  s403,
+  s240,
+  s242,
+  s230,
+  s411,
+  s232,
+  s234,
+  s413,
+  s410,
+  s412,
+  s414,
+  s233,
+  s244,
+  s314,
+  s231,
+  s312,
+  s310,
+  s313,
+  s434,
+  s424,
+  s420,
+  s440,
+  s421,
+  s441,
+  s443,
+  s442,
+  s444,
+  s430,
+  s433,
+  s431,
+  s432,
+  s423,
+  s422,
+  s110,
+  s112,
+  s114,
+  s113,
+  s101,
+  s100,
+  s102,
+  s104,
+  s103,
+  s121,
+  s120,
+  s122,
+  s124,
+  s123,
+  s141,
+  s140,
+  s142,
+  s144,
+  s143,
+  s131,
+  s130,
+  s132,
+  s134,
+  s133,
+  s011,
+  s010,
+  s012,
+  s014,
+  s013,
+  s001,
+  s000,
+  s002,
+  s004,
+  s003,
+  s021,
+  s020,
+  s022,
+  s024,
+  s023,
+  s041,
+  s040,
+  s042,
+  s044,
+  s033,
+  s031,
+  s030,
+  s032,
+  s034,
+  s301,
+  s300,
+  s302,
+  s304,
+  s303,
+  s321,
+  s320,
+  s322,
+  s324,
+  s323,
+  s341,
+  s340,
+  s342,
+  s344,
+  s343,
+  s331,
+  s330,
+  s332,
+  s334,
+  s333,
+  s311
+}
