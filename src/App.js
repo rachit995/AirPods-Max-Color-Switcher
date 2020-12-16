4cdbc415d96556f45d6163d0edb0025b13d3045f
Rachit Sharma <rachit995@rocketmail.com>
2020-12-16 15:45:52 +0530

    ADD: Added tailwind and credits

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { useState } from 'react'
 import './App.css';
 import images from './assets/images'
-import { FaTwitter } from 'react-icons/fa'
+import { FaRandom } from 'react-icons/fa'
+
+function getRandomColor() {
+  return Math.floor(Math.random() * 5)
+}
 
 function App() {
-  const [headPhoneColor, setHeadPhoneColor] = useState(0)
-  const [rightColor, setRightColor] = useState(0)
-  const [leftColor, setLeftColor] = useState(0)
+  const [headPhoneColor, setHeadPhoneColor] = useState(getRandomColor())
+  const [rightColor, setRightColor] = useState(getRandomColor())
+  const [leftColor, setLeftColor] = useState(getRandomColor())
   const imageUrl = images[`s${headPhoneColor}${leftColor}${rightColor}`]
   function generateOptions() {
     return (
@@ -20,47 +24,84 @@ function App() {
     )
   }
   return (
-    <div className='app__container'>
-      <div className="app__leftColumn">
-        <img src={imageUrl} alt='' className='app__AirPodsMaxImage' />
-      </div>
-      <div className="app__rightColumn">
-        <div>
-          <div class="app__lc_new">
-            New
+    <>
+      <div className='container mx-auto'>
+        <div className='grid grid-cols-1 gap-2'>
+
+          <div className='grid grid-cols-1 md:grid-cols-3 gap-2'>
+            <div className="col-span-2">
+              <div class="grid grid-cols-1 gap-2 place-items-center min-w-full p-4">
+                <img src={imageUrl} alt='' className='app__AirPodsMaxImage' />
+              </div>
             </div>
-          <h1 class="app__lc_h1">
-            AirPods Max
-          </h1>
+            <div className=''>
+              <div className="grid grid-cols-1 gap-2 place-items-center h-full p-10 md:p-4">
+                <div>
+                  <div>
+                    <div class="app__lc_new">
+                      New
+                  </div>
+                    <h1 class="text-5xl font-bold py-2">
+                      AirPods Max
+                  </h1>
+                  </div>
+                  <h1 class="app__selectLabel">
+                    Body Color
+                </h1>
+                  <select
+                    className='app__select'
+                    onChange={(e) => { setHeadPhoneColor(e.target.value) }}
+                    value={headPhoneColor}
+                  >
+                    {generateOptions()}
+                  </select>
+                  <h1 class="app__selectLabel">
+                    Left Ear Cushion Color
+                </h1>
+                  <select
+                    className='app__select'
+                    onChange={(e) => { setLeftColor(e.target.value) }}
+                    value={leftColor}
+                  >
+                    {generateOptions()}
+                  </select>
+                  <h1 class="app__selectLabel">
+                    Right Ear Cushion Color
+                </h1>
+                  <select
+                    className='app__select'
+                    onChange={(e) => { setRightColor(e.target.value) }}
+                    value={rightColor}
+                  >
+                    {generateOptions()}
+                  </select>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="p-5 text-center">
+            Color combinations by <a href="https://forums.macrumors.com/threads/all-25-airpod-max-cup-headband-combos.2274186/" className="font-bold" target="_blank" rel="noreferrer">
+              Ryan Barrieau
+            </a> / Developed by  <a href="https://bit.ly/3h2bqRb" className="font-bold" target="_blank" rel="noreferrer">
+              @rachit995
+            </a>
+          </div>
         </div>
-        <h1 class="app__lc_h2">
-          Body Color
-        </h1>
-        <select onChange={(e) => { setHeadPhoneColor(e.target.value) }}>
-          {generateOptions()}
-        </select>
-        <h1 class="app__lc_h2">
-          Left Ear Cushion
-        </h1>
-        <select onChange={(e) => { setLeftColor(e.target.value) }}>
-          {generateOptions()}
-        </select>
-        <h1 class="app__lc_h2">
-          Right Ear Cushion
-        </h1>
-        <select onChange={(e) => { setRightColor(e.target.value) }}>
-          {generateOptions()}
-        </select>
       </div>
-      <div className="app__footer">
-        <a href="https://bit.ly/3h2bqRb" target="_blank" rel="noreferrer">
-          <FaTwitter />
-          <h4>
-            @rachit995
-        </h4>
-        </a>
+      <div className="fixed h-12 w-12 bottom-4 right-4">
+        <button
+          className="p-3 text-lg shadow-md text-gray bg-gray-300 rounded-full"
+          title="Random Color"
+          onClick={() => {
+            setHeadPhoneColor(getRandomColor())
+            setLeftColor(getRandomColor())
+            setRightColor(getRandomColor())
+          }}
+        >
+          <FaRandom />
+        </button>
       </div>
-    </div>
+    </>
   );
 }
 
